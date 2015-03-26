94a54403ebd90a7a40b741edf1825b7ebbe7c076
Takiguchi <takiguchi@TakiPackarbian>
2015-03-26 23:14:42 +0100

    Ajout du script sql permettant de créer la base de données ainsi que ses tables

---
+++ sql/script_bdd.sql
@@ -0,0 +1,38 @@
+-- Création de la bdd
+CREATE DATABASE veilletechno
+  WITH OWNER = "f.thierry"
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'fr_FR.UTF-8'
+       LC_CTYPE = 'fr_FR.UTF-8'
+       CONNECTION LIMIT = -1;
+
+-- Création du shéma
+CREATE SCHEMA veilletechnologique
+  AUTHORIZATION "f.thierry";
+
+-- Sélection du shéma
+SET SCHEMA 'veilletechnologique';
+
+-- Création de la table t_url
+create table t_url(
+	id integer not null,
+	adresse varchar(500) not null,
+	constraint pk_t_url primary key (id)
+);
+
+-- Création de la table t_tag
+create table t_tag(
+	id integer not null,
+	libelle varchar(150) not null,
+	constraint pk_t_tag primary key (id)
+);
+
+-- Création de la table t_ligne_url_tag
+create table t_ligne_url_tag(
+	id_url integer not null,
+	id_tag integer not null,
+	constraint pk_t_ligne_url_tag primary key (id_url,id_tag),
+	constraint fk_t_ligne_url_tag_t_url foreign key (id_url) references t_url (id),
+	constraint fk_t_ligne_url_tag_t_tag foreign key (id_tag) references t_tag (id)
+);
